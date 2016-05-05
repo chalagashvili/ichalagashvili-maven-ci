4b9866f0961918099632a1508f1936a53cae5c90
chalagashvili <ichal13@freeuni.edu.ge>
2016-05-05 12:06:48 +0400

    Create helloworld type program for travis

---
+++ src/main/java/XaiGuyz.java
@@ -0,0 +1,10 @@
+/**
+ * Created by GM on 5/5/2016.
+ */
+public class XaiGuyz {
+
+    public static void main(String[] args){
+        System.out.println("A little party never killed nobody");
+    }
+
+}
